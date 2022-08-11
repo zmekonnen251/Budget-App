5c28b7258c2febe48568a60c06feac5d57401020
Zelalem <mekonnen.zelalem251@gmail.com>
2022-08-11 17:06:31 +0300

    Finalize styling of login and signup pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :update_allowed_params , if: :devise_controller?
+
   protected
 
   def update_allowed_params
-    devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:name, :email, :password, :password_confirmation, :current_password)
-    end
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :current_password])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
 end
