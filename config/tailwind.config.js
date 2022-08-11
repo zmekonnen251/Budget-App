7dcd83b04c697bbb721f58f9138353dd1f2a9e18
Zelalem <mekonnen.zelalem251@gmail.com>
2022-08-11 21:45:16 +0300

    Remove flowbite

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
 		'./app/helpers/**/*.rb',
 		'./app/javascript/**/*.js',
 		'./app/views/**/*.{erb,haml,html,slim}',
-		'./node_modules/flowbite/**/*.js',
 	],
 	theme: {
 		colors: {
@@ -25,6 +24,5 @@ module.exports = {
 		require('@tailwindcss/forms'),
 		require('@tailwindcss/aspect-ratio'),
 		require('@tailwindcss/typography'),
-		require('flowbite/plugin'),
 	],
 };
