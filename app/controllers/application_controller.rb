af2a8ccd5249ac46ca33ac3a7d2930cea8a481ae
Zelalem <mekonnen.zelalem251@gmail.com>
2022-08-12 05:23:24 +0300

    still trying to fix stimulusjs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :update_allowed_params, if: :devise_controller?
 
-  # etag { Rails.application.importmap.digest(resolver: helpers) if request.format&.html? }
-
   
 
   protected
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,7 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from './application';
 
-import HamburgerController from "./hamburger_controller"
-application.register("hamburger", HamburgerController)
+import HamburgerController from './hamburger_controller';
+application.register('hamburger', HamburgerController);
